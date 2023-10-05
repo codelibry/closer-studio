d4758743f0db0a11be282dd3c16603f4fd4257f8
tisukRoman <tisukroman@gmail.com>
2023-10-05 13:13:54 +0300

    feat(content-text.php): use title as a section id for anchor links

--- template-parts/acf-blocks/content-text.php
+++ template-parts/acf-blocks/content-text.php
@@ -1,11 +1,14 @@
 <?php
   $title = get_sub_field('content-text-title');
   $content = get_sub_field('content-text-content');
+
+  // format title as slug to use as section id
+  $section_id = implode('-', explode(' ', strtolower($title)));
 ?>
 
 <?php if ($title and $content): ?>
 
-<section class="section content-text" id="content-text">
+<section class="section content-text" id="<?php echo $section_id ?>">
   <div class="content-text__container">
 
     <h2 class="content-text__title"><?php echo $title ?></h2>
