6bb4f17fc0e74f31980198f391039a1c0f0a39a2
tisukRoman <tisukroman@gmail.com>
2023-10-06 16:41:50 +0300

    feat: add two image layout variants

--- index.php
+++ index.php
@@ -2,17 +2,7 @@
 
 <main class="home">
 
-
-<?php if(have_rows('content', get_option('page_on_front'))): ?>
-
-  <?php while(have_rows('content',  get_option('page_on_front'))): the_row(); ?>
-
-    <?php get_template_part('template-parts/acf-blocks/' . get_row_layout()); ?>
-
-  <?php endwhile; ?>
-
-<?php endif; ?>
-
+  <?php the_content(); ?>
 
 </main>
 
--- template-parts/acf-blocks/gallery.php
+++ template-parts/acf-blocks/gallery.php
@@ -2,6 +2,10 @@
 
   $images = get_sub_field('gallery');
 
+  $gallery_layout = get_sub_field('layout');
+
+  $smaller_image_index = $gallery_layout == 'left_image_smaller' ? 0 : 1;
+
 ?>
 
 <?php if (count($images)): ?>
@@ -11,9 +15,11 @@
 
     <div class="gallery__flex">
 
-      <?php foreach( $images as $image ): ?>
+      <?php foreach( $images as $key => $image ): ?>
 
-        <div class="gallery__box">
+        <?php $small_image_modificator = $key == $smaller_image_index ? 'gallery__box--small' : ''; ?>
+
+        <div class="gallery__box <?php echo $small_image_modificator ?>">
           <?php image_acf($image);?>
         </div>
 
