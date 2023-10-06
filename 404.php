8bd8f3264dcacde59313c82ee06d804b508c4c6b
tisukRoman <tisukroman@gmail.com>
2023-10-06 14:43:16 +0300

    feat: add 404 page

--- 404.php
+++ 404.php
@@ -1,3 +1,16 @@
 <?php get_header();?>
 
+<section class="errorPage">
+  <div class="container">
+    <div class="errorPage__content">
+      <div class="errorPage__textWrapper">
+        <h1 class="errorPage__title">404</h1>
+        <div class="errorPage__subtitle"><?php _e('Oops!', 'closer-studio'); ?></div>
+        <div class="errorPage__text"><?php _e('The Page you are looking for doesn\'t exist', 'closer-studio'); ?></div>
+        <div class="errorPage__button"><a href="<?php echo get_home_url() ?>"><?php _e('Back To Homepage', 'closer-studio'); ?></a></div>
+      </div>
+    </div>
+  </div>
+</section>
+
 <?php get_footer();?>
